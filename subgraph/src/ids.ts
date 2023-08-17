38431a9df6d6b941e9b7e25857f1718b798fc1bd
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-08-17 12:21:25 +0200

    add plugin repos ids

--- subgraph/src/ids.ts
+++ subgraph/src/ids.ts
@@ -1,5 +1,44 @@
 import {Address} from '@graphprotocol/graph-ts';
 
+export function getIdFromStringArray(inputs: string[]): string {
+  return inputs.join('_');
+}
+
+export function getIdFromAddress(address: Address): string {
+  return address.toHexString();
+}
+
 export function getDaoId(dao: Address): string {
-  return dao.toHexString();
+  return getIdFromAddress(dao);
+}
+
+// PluginRepo
+export function getPluginRepoId(pluginRepo: Address): string {
+  return getIdFromAddress(pluginRepo);
+}
+
+export function getPluginSetupId(pluginSetup: Address): string {
+  return getIdFromAddress(pluginSetup);
+}
+
+export function getPluginReleaseId(
+  pluginRepo: Address,
+  release: Number
+): string {
+  return getIdFromStringArray([
+    getPluginRepoId(pluginRepo),
+    release.toString(),
+  ]);
+}
+
+export function getPluginVersionId(
+  pluginRepo: Address,
+  release: Number,
+  build: number
+) {
+  return getIdFromStringArray([
+    getPluginRepoId(pluginRepo),
+    release.toString(),
+    build.toString(),
+  ]);
 }
