dafccc84ae1ab1ef46123c4c2cf8d2749dcd0c70
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-08-28 16:08:17 +0200

    Add test for pluginRepo

--- subgraph/src/ids/dao.ts
+++ subgraph/src/ids/dao.ts
@@ -1,5 +1,11 @@
 import {Address} from '@graphprotocol/graph-ts';
 
+/**
+ * Generates the DAO's ID using its address in hexadecimal format.
+ *
+ * @param daoAddress - The address of the DAO.
+ * @returns A hexadecimal string representation of the provided DAO address.
+ */
 export function getDaoId(daoAddress: Address): string {
   return daoAddress.toHexString();
 }
--- subgraph/tests/ids/dao.test.ts
+++ subgraph/tests/ids/dao.test.ts
@@ -1,26 +1,19 @@
 import {Address} from '@graphprotocol/graph-ts';
-import {getDaoId} from '../src';
+import {getDaoId} from '../../src';
 import {
   afterAll,
   assert,
   beforeAll,
   describe,
-  test,
+  test
 } from 'matchstick-as/assembly/index';
-import {ADDRESS_ONE} from './constants';
+import {ADDRESS_ONE} from '../constants';
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
-describe('Describe id tests', () => {
-  beforeAll(() => {
-    // before all tests
-  });
-
-  afterAll(() => {
-    // after all tests
-  });
-  test('getDaoId', () => {
+describe('DAO ID generation', () => {
+  test('`getDaoId` should return the hexadecimal representation of the provided address', () => {
     const address = Address.fromString(ADDRESS_ONE);
     assert.stringEquals(getDaoId(address), ADDRESS_ONE);
   });
