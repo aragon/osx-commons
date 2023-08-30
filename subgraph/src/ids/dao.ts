e0fd41def0d9fd87ddaf6a03e0ce65074b6c60ea
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-08-30 10:31:19 +0200

    add comment

--- subgraph/src/ids/dao.ts
+++ subgraph/src/ids/dao.ts
@@ -3,9 +3,11 @@ import {Address} from '@graphprotocol/graph-ts';
 /**
  * Generates the DAO's ID using its address in hexadecimal format.
  *
- * @param daoAddress - The address of the DAO.
+ * @param dao - The address of the DAO.
  * @returns A hexadecimal string representation of the provided DAO address.
  */
-export function getDaoId(daoAddress: Address): string {
-  return daoAddress.toHexString();
+export function getDaoId(dao: Address): string {
+  return dao.toHexString();
 }
+
+// TODO: this is not complete, it will be done in it's own task.
