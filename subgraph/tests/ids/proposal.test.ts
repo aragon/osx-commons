90cc57d5762d130111653dd7852cba5aeeb753ad
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-11-10 10:45:34 +0100

    style: applied prettier formatting

--- subgraph/tests/ids/proposal.test.ts
+++ subgraph/tests/ids/proposal.test.ts
@@ -2,13 +2,13 @@ import {
   generateActionEntityId,
   generateProposalEntityId,
   generateTransactionActionsProposalEntityId,
-} from "../../src/ids/proposal";
-import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
-import { assert, describe, test } from "matchstick-as";
-import { ADDRESS_ONE, DUMMY_BYTES32_HEX, DUMMY_PROPOSAL_ID } from "../constants";
-import { bigIntToBytes32 } from "../../src/utils/utils";
+} from '../../src/ids/proposal';
+import {bigIntToBytes32} from '../../src/utils/utils';
+import {ADDRESS_ONE, DUMMY_BYTES32_HEX, DUMMY_PROPOSAL_ID} from '../constants';
+import {Address, BigInt, Bytes} from '@graphprotocol/graph-ts';
+import {assert, describe, test} from 'matchstick-as';
 
-describe("Transaction Actions Proposal ID generation", () => {
+describe('Transaction Actions Proposal ID generation', () => {
   test('`generateActionEntityId` should return the id representation of an action', () => {
     const proposalId = DUMMY_PROPOSAL_ID;
     const index = 0;
@@ -16,8 +16,8 @@ describe("Transaction Actions Proposal ID generation", () => {
     const expectedId = `${proposalId}_${index}`;
 
     assert.stringEquals(generateActionEntityId(proposalId, index), expectedId);
-  })
-  test("`generateTransactionActionsProposalEntityId` should return the id representation of a transaction actions proposal", () => {
+  });
+  test('`generateTransactionActionsProposalEntityId` should return the id representation of a transaction actions proposal', () => {
     const proposalEntityId = generateProposalEntityId(
       Address.fromString(ADDRESS_ONE),
       BigInt.fromI32(1)
