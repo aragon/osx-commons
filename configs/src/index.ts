b7a3bb38f8f9d02c3d3e915660f2be33d0d69222
Mathias Scherer <mathias@aragon.org>
2023-11-29 17:27:29 +0100

    feat(configs): adds supported networks with basic settings feat(configs): adds all deployments with txhash and block number feat(configs): initial setup

---
+++ configs/src/index.ts
@@ -0,0 +1,23 @@
+import * as arbitrum from './deployments/arbitrum.json';
+import * as arbitrumGoerli from './deployments/arbitrumGoerli.json';
+import * as baseGoerli from './deployments/baseGoerli.json';
+import * as baseMainnet from './deployments/baseMainnet.json';
+import * as goerli from './deployments/goerli.json';
+import * as mainnet from './deployments/mainnet.json';
+import * as mumbai from './deployments/mumbai.json';
+import * as polygon from './deployments/polygon.json';
+import * as sepolia from './deployments/sepolia.json';
+import * as networks from './networks.json';
+
+export {
+  networks,
+  arbitrum,
+  arbitrumGoerli,
+  baseGoerli,
+  baseMainnet,
+  goerli,
+  mainnet,
+  mumbai,
+  polygon,
+  sepolia,
+};
