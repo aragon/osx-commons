d795116286085f07b8751b8af1f1b6b608b242c0
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-12-19 19:40:15 +0100

    refactor: removed hardhat time

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -440,3 +440,25 @@ export const OSX_PROPOSAL_ID_REGEX =
 export const HEX_STRING_REGEX = /^(0x)?[0-9a-fA-F]*$/;
 export const ENS_REGEX = /^(?:[a-z0-9-]+\.)*[a-z0-9-]+\.eth$/;
 export const SUBDOMAIN_REGEX = /^[a-z0-9-]+$/;
+
+export const TIME = {
+  SECOND: 1,
+  get MINUTE() {
+    return 60 * this.SECOND;
+  },
+  get HOUR() {
+    return 60 * this.MINUTE;
+  },
+  get DAY() {
+    return 24 * this.HOUR;
+  },
+  get WEEK() {
+    return 7 * this.DAY;
+  },
+  get MONTH() {
+    return 30 * this.DAY;
+  },
+  get YEAR() {
+    return 365 * this.DAY;
+  },
+};
