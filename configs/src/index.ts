ad0c14888b81d7209e530bc901713240385d258c
Mathias Scherer <mathias@aragon.org>
2023-12-21 13:33:04 +0100

    feat(configs): abstract network settings (#34)
    * feat(configs): adds supported networks with basic settings
    feat(configs): adds all deployments with txhash and block number
    feat(configs): initial setup
    
    * feat(configs): adds arbitrum sepolia
    feat(configs): removes arbitrum goerli
    
    * feat(configs): updates all networks and contracts
    
    * build(configs): finishes typescript and eslint config
    
    * fix(configs): adds missing erc20 contracts to polygon
    
    * chore(configs): sorts deployments
    
    * feat(config): adds ethers network aliases
    
    * chore(configs): adds linting to workflow
    
    * chore: fixes formatting
    
    * feat(configs): adds baseSepolia deployment
    
    * fix(configs): renames contracts to a proper formatting
    
    * cicd(configs): adds workflow to publish package
    
    * cicd: only run publish on correct tag
    
    * chore(configs): corrects version number
    
    * chore(configs): corrects license
    
    * deps(configs): updates typescript to 5.2.2
    
    * feat(configs): adds aliases for alchemy subgraphs
    
    * cicd(configs): adds it to the reusable-check-tag workflow
    
    * chore(configs): change to cammelcase

---
+++ configs/src/index.ts
@@ -0,0 +1,25 @@
+import * as arbitrum from './deployments/arbitrum.json';
+import * as arbitrumSepolia from './deployments/arbitrumSepolia.json';
+import * as baseGoerli from './deployments/baseGoerli.json';
+import * as baseMainnet from './deployments/baseMainnet.json';
+import * as baseSepolia from './deployments/baseSepolia.json';
+import * as goerli from './deployments/goerli.json';
+import * as mainnet from './deployments/mainnet.json';
+import * as mumbai from './deployments/mumbai.json';
+import * as polygon from './deployments/polygon.json';
+import * as sepolia from './deployments/sepolia.json';
+import * as networks from './networks.json';
+
+export {
+  networks,
+  arbitrum,
+  arbitrumSepolia,
+  baseGoerli,
+  baseMainnet,
+  baseSepolia,
+  goerli,
+  mainnet,
+  mumbai,
+  polygon,
+  sepolia,
+};
