869e60b351fcb885e61561e55134323480f14819
Michael Heuer <michael.heuer@rwth-aachen.de>
2024-01-04 16:11:54 +0100

    feat: made ProtocolVersion non-abstract

--- contracts/src/utils/versioning/ProtocolVersion.sol
+++ contracts/src/utils/versioning/ProtocolVersion.sol
@@ -6,10 +6,10 @@ import {IProtocolVersion} from "./IProtocolVersion.sol";
 
 /// @title ProtocolVersion
 /// @author Aragon Association - 2023
-/// @notice An abstract, stateless, non-upgradeable contract providing the current Aragon OSx protocol version number.
+/// @notice A stateless, non-upgradeable contract providing the current Aragon OSx protocol version number.
 /// @dev Do not add any new variables to this contract that would shift down storage in the inheritance chain.
 /// @custom:security-contact sirt@aragon.org
-abstract contract ProtocolVersion is IProtocolVersion {
+contract ProtocolVersion is IProtocolVersion {
     // IMPORTANT: Do not add any storage variable, see the above notice.
 
     /// @inheritdoc IProtocolVersion
