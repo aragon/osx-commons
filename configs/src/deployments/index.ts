0e3ea374689c0cb2eeecc0d4e777eeebc5b655ef
Mathias Scherer <mathias@aragon.org>
2024-01-12 15:59:47 +0100

    fix(configs): changes to be commonjs package feat(configs): improves typings for networks feat(configs): adds helper function for networks and deployments

---
+++ configs/src/deployments/index.ts
@@ -0,0 +1,95 @@
+import {
+  NetworkDeployment,
+  NetworkDeploymnets,
+  SupportedNetworks,
+  SupportedVerions,
+} from '../types';
+import * as arbitrum from './arbitrum.json';
+import * as arbitrumSepolia from './arbitrumSepolia.json';
+import * as baseGoerli from './baseGoerli.json';
+import * as baseMainnet from './baseMainnet.json';
+import * as baseSepolia from './baseSepolia.json';
+import * as goerli from './goerli.json';
+import * as mainnet from './mainnet.json';
+import * as mumbai from './mumbai.json';
+import * as polygon from './polygon.json';
+import * as sepolia from './sepolia.json';
+
+/**
+ * Retrieves the network deployments based on the specified network.
+ *
+ * @param {SupportedNetworks} network - The network to retrieve the deployments for.
+ * @return {NetworkDeployments} The network deployments for the specified network.
+ */
+export function getNetworkDeployments(
+  network: SupportedNetworks
+): NetworkDeploymnets {
+  switch (network) {
+    case SupportedNetworks.MAINNET:
+      return mainnet;
+    case SupportedNetworks.GOERLI:
+      return goerli;
+    case SupportedNetworks.SEPOLIA:
+      return sepolia;
+    case SupportedNetworks.POLYGON:
+      return polygon;
+    case SupportedNetworks.MUMBAI:
+      return mumbai;
+    case SupportedNetworks.BASE:
+      return baseMainnet;
+    case SupportedNetworks.BASE_GOERLI:
+      return baseGoerli;
+    case SupportedNetworks.BASE_SEPOLIA:
+      return baseSepolia;
+    case SupportedNetworks.ARBITRUM:
+      return arbitrum;
+    case SupportedNetworks.ARBITRUM_SEPOLIA:
+      return arbitrumSepolia;
+  }
+}
+
+/**
+ * Retrieves the network deployment for a specific version.
+ *
+ * @param {SupportedNetworks} network - The network to retrieve the deployment for.
+ * @param {SupportedVerions} version - The version of the deployment.
+ * @return {NetworkDeployment | undefined} The network deployment for the specified version, or undefined if not found.
+ */
+export function getNetworkDeploymentForVersion(
+  network: SupportedNetworks,
+  version: SupportedVerions
+): NetworkDeployment | undefined {
+  return getNetworkDeployments(network)[version];
+}
+
+/**
+ * Retrieves the latest network deployment for the specified network.
+ *
+ * @param {SupportedNetworks} network - The network to retrieve the deployment for.
+ * @return {NetworkDeployment | undefined} The latest network deployment, or undefined if not found.
+ */
+export function getLatestNetworkDeployment(
+  network: SupportedNetworks
+): NetworkDeployment | undefined {
+    const versions = Object.values(SupportedVerions).reverse();
+    for(const version of versions) {
+        const deployment = getNetworkDeploymentForVersion(network, version);
+        if(deployment) {
+            return deployment;
+        }
+    }
+    return undefined
+}
+
+export {
+  mainnet,
+  goerli,
+  sepolia,
+  polygon,
+  mumbai,
+  baseMainnet,
+  baseGoerli,
+  baseSepolia,
+  arbitrum,
+  arbitrumSepolia,
+};
--- configs/src/types.ts
+++ configs/src/types.ts
@@ -2,6 +2,8 @@ export type NetworkConfig = {
   url: string;
   isTestnet: boolean;
   chainId: number;
+  feesUrl?: string;
+  gasPrice?: number;
   aliases: NetworkAliases;
 };
 
@@ -12,5 +14,39 @@ export type NetworkAliases = {
 };
 
 export type NetworkConfigs<T = NetworkConfig> = {
-  [network: string]: T;
+  [network in SupportedNetworks]: T;
+};
+
+export enum SupportedNetworks {
+  MAINNET = 'mainnet',
+  GOERLI = 'goerli',
+  SEPOLIA = 'sepolia',
+  POLYGON = 'polygon',
+  MUMBAI = 'mumbai',
+  BASE = 'baseMainnet',
+  BASE_GOERLI = 'baseGoerli',
+  BASE_SEPOLIA = 'baseSepolia',
+  ARBITRUM = 'arbitrum',
+  ARBITRUM_SEPOLIA = 'arbitrumSepolia',
+}
+
+// the entries in this enum has to be in order from
+// oldest to newest so that getLatestNetworkVersion() works as expected
+export enum SupportedVerions {
+  V100 = 'v1.0.0',
+  V130 = 'v1.3.0',
+}
+
+export type NetworkDeploymnets = {
+  [index in SupportedVerions]?: NetworkDeployment;
+};
+
+export type NetworkDeployment = {
+  [index: string]: ContractDeployment;
+};
+
+export type ContractDeployment = {
+  address: string;
+  blockNumber: number;
+  deploymentTx: string;
 };
