db11b9be2c067dab1848945082264d20d8c1076d
Michael Heuer <michael.heuer@rwth-aachen.de>
2024-01-16 16:34:20 +0100

    test: use fixtures and re-use test for upgradeable proposal base class

--- contracts/test/plugin/extensions/proposal.ts
+++ contracts/test/plugin/extensions/proposal.ts
@@ -5,7 +5,9 @@ import {
   IDAO__factory,
   IProposal__factory,
   ProposalMock,
+  ProposalUpgradeableMock,
   ProposalMock__factory,
+  ProposalUpgradeableMock__factory,
 } from '../../../typechain';
 import {ExecutedEvent} from '../../../typechain/src/dao/IDAO';
 import {erc165ComplianceTests} from '../../helpers';
@@ -16,19 +18,11 @@ import {
   getInterfaceId,
 } from '@aragon/osx-commons-sdk';
 import {IProposal__factory as IProposal_V1_0_0__factory} from '@aragon/osx-ethers-v1.0.0';
-import {time} from '@nomicfoundation/hardhat-network-helpers';
+import {loadFixture, time} from '@nomicfoundation/hardhat-network-helpers';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 import {expect} from 'chai';
 import {BigNumberish, BytesLike} from 'ethers';
 import {ethers} from 'hardhat';
-
-describe('IProposal', function () {
-  it('has the same interface ID as its initial version introduced in v1.0.0', async () => {
-    const current = getInterfaceId(IProposal__factory.createInterface());
-    const initial = getInterfaceId(IProposal_V1_0_0__factory.createInterface());
-    expect(current).to.equal(initial);
-  });
-});
 
 export type ProposalData = {
   metadata: BytesLike;
@@ -38,44 +32,40 @@ export type ProposalData = {
   allowFailureMap: BigNumberish;
 };
 
+describe('IProposal', function () {
+  it('has the same interface ID as its initial version introduced in v1.0.0', async () => {
+    const current = getInterfaceId(IProposal__factory.createInterface());
+    const initial = getInterfaceId(IProposal_V1_0_0__factory.createInterface());
+    expect(current).to.equal(initial);
+  });
+});
+
 describe('Proposal', async () => {
-  let alice: SignerWithAddress;
-  let bob: SignerWithAddress;
-  let exampleData: ProposalData;
+  proposalBaseTests(proposalFixture);
+});
 
-  let daoMock: DAOMock;
-  let contract: ProposalMock;
+describe('ProposalUpgradeable', async () => {
+  proposalBaseTests(proposalUpgradeableFixture);
 
-  before(async () => {
-    [alice, bob] = await ethers.getSigners();
-    daoMock = await new DAOMock__factory(alice).deploy();
-
-    const uri = ethers.utils.hexlify(
-      ethers.utils.toUtf8Bytes(
-        'ipfs://QmTMcfhxgYA3ziwpnhEg1K3aFn7ixMH9dBxWXs5YTJdZwR'
-      )
-    );
-    const current = await time.latest();
-
-    exampleData = {
-      metadata: uri,
-      startDate: current,
-      endDate: current + 12,
-      actions: [],
-      allowFailureMap: 0,
-    };
+  it.skip('upgrades', async () => {
+    expect(true).to.equal(false);
   });
 
-  beforeEach(async () => {
-    contract = await new ProposalMock__factory(alice).deploy();
+  it.skip('can be reinitialized', async () => {
+    expect(true).to.equal(false);
   });
+});
 
+// Contains tests for functionality common for `ProposalMock` and `ProposalUpgradeableMock` to avoid duplication.
+function proposalBaseTests(fixture: () => Promise<ProposalFixtureInput>) {
   it('counts proposals', async () => {
-    expect(await contract.proposalCount()).to.equal(0);
+    const {alice, bob, proposalMock, exampleData} = await loadFixture(fixture);
+
+    expect(await proposalMock.proposalCount()).to.equal(0);
 
     const creator = bob;
 
-    await contract
+    await proposalMock
       .connect(alice)
       .createProposal(
         creator.address,
@@ -86,21 +76,24 @@ describe('Proposal', async () => {
         exampleData.allowFailureMap
       );
 
-    expect(await contract.proposalCount()).to.equal(1);
+    expect(await proposalMock.proposalCount()).to.equal(1);
   });
 
   it('creates proposalIds', async () => {
+    const {proposalMock} = await loadFixture(fixture);
     const expectedProposalId = 0;
-    const proposalId = await contract.callStatic.createProposalId();
+    const proposalId = await proposalMock.callStatic.createProposalId();
 
     expect(proposalId).to.equal(expectedProposalId);
   });
 
   it('creates proposals', async () => {
+    const {alice, bob, proposalMock, exampleData} = await loadFixture(fixture);
+
     const expectedProposalId = 0;
     const creator = bob;
 
-    const proposalId = await contract
+    const proposalId = await proposalMock
       .connect(alice)
       .callStatic.createProposal(
         creator.address,
@@ -115,11 +108,13 @@ describe('Proposal', async () => {
   });
 
   it('emits the `ProposalCreated` event', async () => {
+    const {alice, bob, proposalMock, exampleData} = await loadFixture(fixture);
+
     const expectedProposalId = 0;
     const creator = bob;
 
     await expect(
-      contract
+      proposalMock
         .connect(alice)
         .createProposal(
           creator.address,
@@ -130,7 +125,7 @@ describe('Proposal', async () => {
           exampleData.allowFailureMap
         )
     )
-      .to.emit(contract, IPROPOSAL_EVENTS.PROPOSAL_CREATED)
+      .to.emit(proposalMock, IPROPOSAL_EVENTS.PROPOSAL_CREATED)
       .withArgs(
         expectedProposalId,
         creator.address,
@@ -143,11 +138,15 @@ describe('Proposal', async () => {
   });
 
   it('executes proposals', async () => {
+    const {alice, daoMock, proposalMock, exampleData} = await loadFixture(
+      fixture
+    );
+
     const expectedExecResults: string[] = [];
     const expectedFailureMap: BigNumberish = 0;
 
     const proposalId = 0;
-    const [execResults, failureMap] = await contract
+    const [execResults, failureMap] = await proposalMock
       .connect(alice)
       .callStatic.executeProposal(
         daoMock.address,
@@ -161,10 +160,14 @@ describe('Proposal', async () => {
   });
 
   it('emits the `ProposalExecuted` event', async () => {
+    const {alice, daoMock, proposalMock, exampleData} = await loadFixture(
+      fixture
+    );
+
     const proposalId = 0;
 
     await expect(
-      contract
+      proposalMock
         .connect(alice)
         .executeProposal(
           daoMock.address,
@@ -173,22 +176,26 @@ describe('Proposal', async () => {
           exampleData.allowFailureMap
         )
     )
-      .to.emit(contract, IPROPOSAL_EVENTS.PROPOSAL_EXECUTED)
+      .to.emit(proposalMock, IPROPOSAL_EVENTS.PROPOSAL_EXECUTED)
       .withArgs(proposalId);
   });
 
   it('emits the `Executed` event on the executing DAO', async () => {
+    const {alice, daoMock, proposalMock, exampleData} = await loadFixture(
+      fixture
+    );
+
     const proposalId = 0;
     const proposalIdAsBytes32 = ethers.utils.hexZeroPad(
       ethers.utils.hexlify(proposalId),
       32
     );
 
-    const expectedActor = contract.address;
+    const expectedActor = proposalMock.address;
     const expectedExecResults: string[] = [];
     const expectedFailureMap: BigNumberish = 0;
 
-    const tx = await contract
+    const tx = await proposalMock
       .connect(alice)
       .executeProposal(
         daoMock.address,
@@ -212,25 +219,70 @@ describe('Proposal', async () => {
 
   describe('ERC-165', async () => {
     it('supports the `ERC-165` standard', async () => {
-      await erc165ComplianceTests(contract, alice);
+      const {alice, proposalMock} = await loadFixture(fixture);
+      await erc165ComplianceTests(proposalMock, alice);
     });
 
     it('supports the `IProposal` interface', async () => {
+      const {proposalMock} = await loadFixture(fixture);
       const iface = IProposal__factory.createInterface();
-      expect(await contract.supportsInterface(getInterfaceId(iface))).to.be
+      expect(await proposalMock.supportsInterface(getInterfaceId(iface))).to.be
         .true;
     });
   });
-});
+}
 
-describe.skip('ProposalUpgradeable', async () => {
-  // TODO run the same tests as for `DaoAuthorizable`.
+type BaseFixtureInput = {
+  alice: SignerWithAddress;
+  bob: SignerWithAddress;
+  daoMock: DAOMock;
+  exampleData: ProposalData;
+};
 
-  it('upgrades', async () => {
-    expect(true).to.equal(false);
-  });
+async function baseFixture(): Promise<BaseFixtureInput> {
+  const [alice, bob] = await ethers.getSigners();
+  const daoMock = await new DAOMock__factory(alice).deploy();
 
-  it('can be reinitialized', async () => {
-    expect(true).to.equal(false);
-  });
-});
+  const uri = ethers.utils.hexlify(
+    ethers.utils.toUtf8Bytes(
+      'ipfs://QmTMcfhxgYA3ziwpnhEg1K3aFn7ixMH9dBxWXs5YTJdZwR'
+    )
+  );
+  const current = await time.latest();
+
+  const exampleData = {
+    metadata: uri,
+    startDate: current,
+    endDate: current + 12,
+    actions: [],
+    allowFailureMap: 0,
+  };
+
+  return {alice, bob, daoMock, exampleData};
+}
+
+type ProposalFixtureInput = {
+  proposalMock: ProposalMock | ProposalUpgradeableMock;
+  alice: SignerWithAddress;
+  bob: SignerWithAddress;
+  daoMock: DAOMock;
+  exampleData: ProposalData;
+};
+
+async function proposalFixture(): Promise<ProposalFixtureInput> {
+  const {alice, bob, daoMock, exampleData} = await baseFixture();
+
+  const proposalMock = await new ProposalMock__factory(alice).deploy();
+
+  return {alice, bob, proposalMock, daoMock, exampleData};
+}
+
+async function proposalUpgradeableFixture(): Promise<ProposalFixtureInput> {
+  const {alice, bob, daoMock, exampleData} = await baseFixture();
+
+  const proposalMock = await new ProposalUpgradeableMock__factory(
+    alice
+  ).deploy();
+
+  return {alice, bob, proposalMock, daoMock, exampleData};
+}
