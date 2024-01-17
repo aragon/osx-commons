de23898fccd4c74eb4ba1bcbb9dfa1832a9ce106
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2024-01-17 12:07:45 +0100

    add helper function

--- configs/src/utils.ts
+++ configs/src/utils.ts
@@ -1,9 +1,32 @@
 import {networks} from '.';
 import {SupportedNetworks, SupportedAliases} from './types';
 
+/**
+ *
+ *
+ * @export
+ * @param {SupportedNetworks} network
+ * @param {SupportedAliases} alias
+ * @return {*}  {string}
+ */
 export function getNetworkAlias(
   network: SupportedNetworks,
   alias: SupportedAliases
 ): string {
   return networks[network].aliases[alias] || network;
+}
+/**
+ *
+ *
+ * @export
+ * @param {string} alias
+ * @param {SupportedAliases} aliasName
+ * @return {*}  {(SupportedNetworks | undefined)}
+ */
+export function getNetworkNameFromAlias(
+  alias: string,
+  aliasName: SupportedAliases
+): SupportedNetworks | undefined {
+  const supportedNetworks = Object.keys(networks) as SupportedNetworks[];
+  return supportedNetworks.find(n => networks[n].aliases[aliasName] === alias);
 }
---
+++ configs/test/unit/utils.test.ts
@@ -0,0 +1,12 @@
+describe("utils", () => {
+    describe("getNetworkAlias",() => {
+        it("should return the network alias", () => {
+            // TODO
+        });
+    })
+    describe("getNetworkNameFromAlias",() => {
+        it("should return the network name from alias", () => {
+            // TODO
+        });
+    })
+});
