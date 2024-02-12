3e1b4ae91d57a8650eb4aea39046debca96cd4fc
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2024-02-12 15:27:59 +0100

    fix imports

--- configs/src/deployments/contracts.ts
+++ configs/src/deployments/contracts.ts
@@ -1,14 +1,14 @@
 import {SupportedNetworks} from '../networks';
-import * as arbitrum from './deployments/arbitrum.json';
-import * as arbitrumSepolia from './deployments/arbitrumSepolia.json';
-import * as baseGoerli from './deployments/baseGoerli.json';
-import * as baseMainnet from './deployments/baseMainnet.json';
-import * as baseSepolia from './deployments/baseSepolia.json';
-import * as goerli from './deployments/goerli.json';
-import * as mainnet from './deployments/mainnet.json';
-import * as mumbai from './deployments/mumbai.json';
-import * as polygon from './deployments/polygon.json';
-import * as sepolia from './deployments/sepolia.json';
+import * as arbitrum from './json/arbitrum.json';
+import * as arbitrumSepolia from './json/arbitrumSepolia.json';
+import * as baseGoerli from './json/baseGoerli.json';
+import * as baseMainnet from './json/baseMainnet.json';
+import * as baseSepolia from './json/baseSepolia.json';
+import * as goerli from './json/goerli.json';
+import * as mainnet from './json/mainnet.json';
+import * as mumbai from './json/mumbai.json';
+import * as polygon from './json/polygon.json';
+import * as sepolia from './json/sepolia.json';
 import {NetworkDeployment, SupportedVersions} from './types';
 
 export {
