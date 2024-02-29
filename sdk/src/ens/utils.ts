d3ffa406e2fd29caca87d30d6f7eeb9d63eb4333
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2024-02-29 11:39:44 +0100

    fix: provider type and changelog

--- sdk/src/ens/utils.ts
+++ sdk/src/ens/utils.ts
@@ -2,28 +2,28 @@ import {InvalidEnsError, UnsupportedNetworkError} from '../errors';
 import {isEnsName} from '../validation';
 import {getNetworkByAlias} from '@aragon/osx-commons-configs';
 import {Networkish} from '@ethersproject/networks';
-import {JsonRpcProvider} from '@ethersproject/providers';
+import {JsonRpcProvider, Provider} from '@ethersproject/providers';
 
 /**
  * Resolves an ENS name to an address given a provider
  *
  * @export
  * @param {string} ensName
- * @param {JsonRpcProvider} provider
+ * @param {Provider | Networkish} providerOrNetwork
  * @return {(Promise<string | null>)}
  */
 export function resolveEnsName(
   ensName: string,
-  providerOrNetwork: JsonRpcProvider | Networkish
+  providerOrNetwork: Provider | Networkish
 ): Promise<string | null> {
   // check if the ensName is valid
   if (!isEnsName(ensName)) {
     throw new InvalidEnsError(ensName);
   }
-  let provider: JsonRpcProvider;
+  let provider: Provider;
   // check if the providerOrNetwork is a provider or a network
   // if it's a provider, use it
-  if (providerOrNetwork instanceof JsonRpcProvider) {
+  if (providerOrNetwork instanceof Provider) {
     provider = providerOrNetwork;
     // any other case, assume it's a network and create a provider
   } else {
