18bbbb760f568117f8789875199783c62d3e2f07
Claudia <claudiabarcelovaldes40@gmail.com>
2024-03-08 21:01:08 +0100

    ci: remove not needed console logs

--- configs/src/networks/initializer.ts
+++ configs/src/networks/initializer.ts
@@ -1,8 +1,5 @@
 export let rpcApiKey: string;
 
 export function init(apiKey: string) {
-  // Log the received environment variables
-  console.log('Received environment variables:');
-  console.log('API Key:', apiKey);
   rpcApiKey = apiKey;
 }
