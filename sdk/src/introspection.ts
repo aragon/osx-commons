b3bd6dad809c3e59ced8af9c1ba619ce4deca311
josemarinas <36479864+josemarinas@users.noreply.github.com>
2024-04-22 16:37:23 +0200

    feat(sdk): add `getProtocolVersion` function (#76)
    * add: getProtocolVersion function
    
    * fix: prettier
    
    * fix: linting
    
    * Update sdk/src/introspection.ts
    
    Co-authored-by: Carles <75954325+banasa44@users.noreply.github.com>
    
    * Update sdk/src/introspection.ts
    
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>
    
    * Update sdk/test/unit/introspection.test.ts
    
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>
    
    * remove: constant ADDRESS_ONE
    
    * fix: prettier
    
    * update: changelog and package version
    
    * fix: export of addres one
    
    * fix: update getProtocolVersion return comment
    
    * chore: add ADDRESS_ONE to constants
    
    ---------
    
    Co-authored-by: Carles <75954325+banasa44@users.noreply.github.com>
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- sdk/src/introspection.ts
+++ sdk/src/introspection.ts
@@ -1,5 +1,9 @@
+import {InvalidAddressError} from './errors';
 import {Interface} from '@ethersproject/abi';
+import {isAddress} from '@ethersproject/address';
 import {BigNumber} from '@ethersproject/bignumber';
+import {Contract} from '@ethersproject/contracts';
+import {JsonRpcProvider} from '@ethersproject/providers';
 
 /**
  * Gets the interfaceId of a given interface
@@ -15,4 +19,38 @@ export function getInterfaceId(iface: Interface): string {
     interfaceId = interfaceId.xor(iface.getSighash(func));
   }
   return interfaceId.toHexString();
+}
+
+/**
+ * Gets the protocol version of a contract, if the contract does not have a
+ * protocolVersion function, it will return [1, 0, 0]
+ *
+ * @export
+ * @param {string} rpc
+ * @param {string} contractAddress
+ * @return {*}  {Promise<[number, number, number]>}
+ */
+export async function getProtocolVersion(
+  rpc: string,
+  contractAddress: string
+): Promise<[number, number, number]> {
+  if (!isAddress(contractAddress)) {
+    throw new InvalidAddressError(contractAddress);
+  }
+  const provider = new JsonRpcProvider(rpc);
+  const iface = new Interface([
+    'function protocolVersion() public pure returns (uint8[3] memory)',
+  ]);
+  const contract = new Contract(contractAddress, iface, provider);
+  let version: [number, number, number];
+  try {
+    version = await contract.protocolVersion();
+  } catch (e) {
+    // version 1.0.0 of the contract does not have a protocolVersion function
+    // so if we receive an error we cannot differentiate between a call exception
+    // and a contract that does not have the function. So we assume that is
+    // a version 1.0.0 contract that does not have the function and return [1, 0, 0]
+    version = [1, 0, 0];
+  }
+  return version;
 }
