8135b8f2b3672d42dd9873420dd597ca1046bc74
Claudia <claudiabarcelovaldes40@gmail.com>
2024-08-21 18:52:13 +0200

    feat: refactor code to avoid duplications

--- contracts/src/plugin/Plugin.sol
+++ contracts/src/plugin/Plugin.sol
@@ -106,35 +106,14 @@ abstract contract Plugin is IPlugin, ERC165, DaoAuthorizable, ProtocolVersion {
         IDAO.Action[] memory _actions,
         uint256 _allowFailureMap
     ) internal virtual returns (bytes[] memory execResults, uint256 failureMap) {
-        Operation op = currentTargetConfig.operation;
-
-        if (op == Operation.DelegateCall) {
-            bool success;
-            bytes memory data;
-
-            (success, data) = currentTargetConfig.target.delegatecall(
-                abi.encodeCall(IDAO.execute, (_callId, _actions, _allowFailureMap))
-            );
-
-            if (!success) {
-                if (data.length > 0) {
-                    assembly {
-                        let returndata_size := mload(data)
-                        revert(add(32, data), returndata_size)
-                    }
-                } else {
-                    revert ExecuteFailed();
-                }
-            }
-
-            (execResults, failureMap) = abi.decode(data, (bytes[], uint256));
-        } else {
-            (execResults, failureMap) = IDAO(currentTargetConfig.target).execute(
+        return
+            _execute(
+                currentTargetConfig.target,
                 _callId,
                 _actions,
-                _allowFailureMap
+                _allowFailureMap,
+                currentTargetConfig.operation
             );
-        }
     }
 
     /// @notice Forwards the actions to the `target` for the execution.
@@ -169,6 +148,7 @@ abstract contract Plugin is IPlugin, ERC165, DaoAuthorizable, ProtocolVersion {
                     revert ExecuteFailed();
                 }
             }
+            (execResults, failureMap) = abi.decode(data, (bytes[], uint256));
         } else {
             (execResults, failureMap) = IDAO(_target).execute(_callId, _actions, _allowFailureMap);
         }
