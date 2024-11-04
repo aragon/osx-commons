0f6ff1037fa88d4f328b467b75cee3e08e51b613
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-11-04 16:12:36 +0400

    remove .only

--- contracts/test/executors/executor.ts
+++ contracts/test/executors/executor.ts
@@ -50,7 +50,7 @@ export async function getActions() {
   };
 }
 
-describe.only('Executor', async () => {
+describe('Executor', async () => {
   let data: any;
   let executor: Executor;
   let ownerAddress: string;
@@ -65,7 +65,7 @@ describe.only('Executor', async () => {
   });
 
   describe('ERC-165', async () => {
-    it.only('supports the `ERC-165` standard', async () => {
+    it('supports the `ERC-165` standard', async () => {
       await erc165ComplianceTests(executor, signers[0]);
     });
 
