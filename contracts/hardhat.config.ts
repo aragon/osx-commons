5c56749ef80528074833ad68b9ad92ba3eca44ba
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2024-11-28 14:36:07 +0100

    Update contracts/hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -142,7 +142,7 @@ const config: HardhatUserConfig = {
       // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
         enabled: true,
-        runs: 2000,
+        runs: 800,
       },
     },
   },
