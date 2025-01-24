337c2ca0d19e488b1fc55d4446a5bdac9d0542ab
Claudia <claudiabarcelovaldes40@gmail.com>
2025-01-24 19:59:25 +0100

    feat: modify the wagmi template to get generate only the deised contracts

--- artifacts/wagmi.foundry.config.ts
+++ artifacts/wagmi.foundry.config.ts
@@ -1,13 +1,21 @@
 import {defineConfig} from '@wagmi/cli';
 import {foundry} from '@wagmi/cli/plugins';
 
+// CONTRACTS_TO_INCLUDE is needed because foundry has not folder structure in the artifacts,
+// if this is not defined it will generate the type of all the compiled contracts in the out folder
+// check here https://github.com/foundry-rs/foundry/issues/1573
+
 export default defineConfig({
   // using abi instead of generated to avoid being ignored by git
   out: 'generated/abis.ts',
   plugins: [
     foundry({
       project: './',
-      exclude: ['**/test/**', '**/mocks/**'],
+      include: (() => {
+        return JSON.parse(process.env.CONTRACTS_TO_INCLUDE || '[]').map(
+          (contractName: string) => `${contractName}.json`
+        );
+      })(),
     }),
   ],
 });
