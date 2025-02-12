4b95f2dff9a9e7d3d4e37416ace9d4145d82f196
Claudia <claudiabarcelovaldes40@gmail.com>
2025-02-12 11:24:17 +0100

    fix: formatting and change the content var type

--- sdk/src/ipfs.ts
+++ sdk/src/ipfs.ts
@@ -7,12 +7,14 @@ const IPFS_URI_REGEX =
   /^ipfs:\/\/((Qm[1-9A-HJ-NP-Za-km-z]{44,})|(b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,})|(z[1-9A-HJ-NP-Za-km-z]{48,})|(F[0-9A-F]{50,}))$/;
 
 function buildFileName(fileName: string): string {
-  fileName = fileName.replaceAll(/[^a-zA-Z0-9.-]/g, "-").replaceAll(/[-]+/g, "-")
+  fileName = fileName
+    .replaceAll(/[^a-zA-Z0-9.-]/g, '-')
+    .replaceAll(/[-]+/g, '-');
   return `osx-${fileName}.json`;
 }
 
 export async function uploadToPinata(
-  content: string,
+  content: any,
   fileName: string,
   pinataJWT: string
 ): Promise<string> {
